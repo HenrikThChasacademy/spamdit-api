9ff9a9a9b1cf43a2c2f8f65046181dbbeb877596
Henrik Theolin <henrik.theolin@alten.se>
2020-11-05 10:07:21 +0100

    Addd inmemory db and correct ecr address

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
+	implementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
